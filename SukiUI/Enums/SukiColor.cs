0330d66b821a3c0d8574ee80056eb51f30d83ae5
Samuel Aleks <samuel.j.aleks@gmail.com>
2026-03-30 15:29:49 -0500

    New themes

--- SukiUI/Enums/SukiColor.cs
+++ SukiUI/Enums/SukiColor.cs
@@ -2,5 +2,7 @@ namespace SukiUI.Enums;
 
 public enum SukiColor
 {
-    Blue, Green, Red, Orange
+    Blue, Green, Red, Orange, Purple, Teal, Pink, Yellow, Indigo,
+    Grayscale, Nord, Rosepine, Dracula, Monokai, TokyoNight, Everforest, Kanagawa, Mocha,
+    Gruvbox, Solarized, Ayu, OneDark
 }
